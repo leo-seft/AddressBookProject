47e3d175b4add8c06297e9e5d3a8fcf5c77ab7be
leo-seft <leosefton13@hotmail.com>
2024-12-09 12:20:23 +0000

    adding class for contacts and creating SQL database

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+
+namespace TaskManagement{
+    class Program{
+        static void Main() {
+        string connectionString = "Data Source=AddressBook.db";
+
+        using (var connection = new SqliteConnection(connectionString)) {
+            connection.Open();
+
+            string createTableQuery = @"
+                CREATE TABLE IF NOT EXISTS Contacts (
+                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    FirstName TEXT NOT NULL,
+                    LastName TEXT NOT NULL,
+                    PhoneNumber TEXT NOT NULL UNIQUE,
+                    Email TEXT UNIQUE,
+                    Address TEXT
+                );";
+
+            using (var command = new SqliteCommand(createTableQuery, connection)) {
+                command.ExecuteNonQuery();
+                Console.WriteLine("Database and table created successfully.");
+            }
+        }
+    }
+    public class Contact {
+    public int ID { get; set; }
+    public string FirstName { get; set; } 
+    public string LastName { get; set; }  
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+}
+
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 **Address Book Project:**
 
-- Build a program to manage contacts (add, edit, delete) CRUD.
+- Build a program to manage contacts (add, edit, delete) CRUD
+- Use SQL for database
 - Use database I/O for data storage and exception handling.
+
+1.  - Create class for contact
+    - Create SQL database
+2.  Create CRUD options
+    - Create contact
+    - Read contact
+    - Update contact
+    - Delete contact
+
+Bonuses:
+
+- Sort
+- Search
